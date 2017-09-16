4cc44490c2722737d773e73f34c6ea9f10465577
iamninja <bagios234@gmail.com>
2017-09-16 04:37:57 +0300

    Add user-form component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
+import { UserFormComponent } from './user-form/user-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserProfileComponent,
+    UserFormComponent,
   ],
   imports: [
     BrowserModule,
