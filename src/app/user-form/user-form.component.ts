9acb5044f0222359c10d044877880d5c27a688fd
iamninja <bagios234@gmail.com>
2017-09-16 05:03:03 +0300

    Add validation on user-form.

--- src/app/user-form/user-form.component.ts
+++ src/app/user-form/user-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+
 import { User } from '../shared/models/user';
 
 @Component({
@@ -8,9 +9,20 @@ import { User } from '../shared/models/user';
 })
 export class UserFormComponent implements OnInit {
 
+  newUser: User = new User();
+  active: boolean = true;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSubmit() {
+    console.log(this.newUser);
+
+    this.newUser = new User();
+    // Workaround to reset the form after submit
+    this.active = false;
+    setTimeout(() => this.active = true, 0);
+  }
 }
